ab879af6d6a09324983d04a14f7bb31e12f3c4e1
pyry.rautalin@gmail.com <pypy@DESKTOP-B9BO8U0>
2025-04-06 16:51:19 +0300

    Change to arrow functions and add navigation elements

--- index.jsx
+++ index.jsx
@@ -1,35 +1,40 @@
 import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
-function Header() {
-    return (
-        <header>
-                <img src="react-logo.png" width="40px" alt="React logo" />
-            </header>
-    )
 
-}
+const Header = () => ( 
+    <header>
+        <nav>
+        <img src="react-logo.png" width="40px" alt="React logo" />
+            <ul className="topnav">         
+                <li>Pricing</li>
+                <li>About </li>
+                <li>Contact</li>
+            </ul>  
+        </nav>
+        
+    </header>
+)
 
-function MainContent() {
-    return (
+    
+
+const MainContent = () => (
     <main>
     <h1>Reason I am excited to learn React</h1>
     <ol>
         <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-        <li>I am more likely to get a job as a front end developer if I know React</li>
+        <li>I am more likely to get a job as a front end developer when I know React</li>
     </ol>
 </main>
     )
 
-}
 
-function Footer() {
-    return (
+const Footer = () => (
         <footer>
                 <small>© 2025 Pyry R</small>
             </footer>
     )
-}
+
 
 function Page() {
     return (
